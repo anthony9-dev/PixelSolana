0ed03da1a2a2af5639420f974f6fa848ba79df30
unknown <anthony.coquelin@lp2i-poitiers.fr>
2025-06-05 15:09:25 +0200

    MAJ 7.0

--- game.js
+++ game.js
@@ -67,7 +67,7 @@ canvas.addEventListener('click', (event) => {
   const tile = map[y][x];
   
   if(tile === 0) {
-    alert("Case vide : tu peux construir ici !");
+    alert("Case vide : tu peux construire ici !");
   } else {
     alert(`Bâtiment niveau ${tile} à cette position.`);
   }
